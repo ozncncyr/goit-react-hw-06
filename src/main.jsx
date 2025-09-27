3ebbec35e12c8742e96ec981d8bdb957caab1305
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-09-27 23:49:24 +0300

    Redux-Persist added

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { MoonLoader } from "react-spinners";
-// import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store} loading={<MoonLoader />}>
-    <App />
+  <Provider store={store}>
+    <PersistGate
+      loading={<MoonLoader color="#007bff" size={40} />}
+      persistor={persistor}
+    >
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import contactReducer from "./reducers/contactsSlice";
 import filterReducer from "./reducers/filtersSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  contacts: contactReducer,
+  filters: filterReducer,
+});
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+  whitelist: ["contacts"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactReducer,
-    filters: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);

aa14eaa5ebbcf8ca895a196c3d5ee1fc4b1a2644
ozncncyr <ozncncyr@outlook.com>
2026-03-03 22:47:46 +0300

    Refactor quotes for consistency in contactsSlice.js

--- src/redux/reducers/contactsSlice.js
+++ src/redux/reducers/contactsSlice.js
@@ -1,16 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   items: [
-    { id: "id-1", name: "Rosie Simpson", number: "4591256" },
-    { id: "id-2", name: "Hermione Kline", number: "4438912" },
-    { id: "id-3", name: "Eden Clements", number: "6451779" },
-    { id: "id-4", name: "Annie Copeland", number: "2279126" },
+    { id: 'id-1', name: 'Rosie Simpson', number: '4591256' },
+    { id: 'id-2', name: 'Hermione Kline', number: '4438912' },
+    { id: 'id-3', name: 'Eden Clements', number: '6451779' },
+    { id: 'id-4', name: 'Annie Copeland', number: '2279126' },
   ],
 };
 
 export const contactsSlice = createSlice({
-  name: "contacts",
+  name: 'contacts',
   initialState,
   reducers: {
     addContact(state, action) {
@@ -18,17 +18,17 @@ export const contactsSlice = createSlice({
     },
     deleteContact(state, action) {
       state.items = state.items.filter(
-        (contact) => contact.id !== action.payload
+        contact => contact.id !== action.payload
       );
     },
   },
 });
 
-export const selectContacts = (state) => {
+export const selectContacts = state => {
   const search = state.filters.search.toLowerCase();
   if (search) {
     return state.contacts.items.filter(
-      (contact) =>
+      contact =>
         contact.name.toLowerCase().includes(search) ||
         contact.number.includes(state.filters.search)
     );
